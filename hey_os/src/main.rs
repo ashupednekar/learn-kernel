fa95f0947554f0604bf9e0490c0b6ba7e201caac
ashupednekar <ashutoshpednekar15@gmail.com>
2026-06-23 20:22:49 +0530

    feat: hello fkn world :)

--- hey_os/src/main.rs
+++ hey_os/src/main.rs
@@ -1,8 +1,19 @@
 #![no_std]
 #![no_main]
+
+use core::isize;
 mod panic;
+
+static HELLO: &[u8] = b"Hello World!";
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> !{
-    loop{}
+    let vga_buf = 0xb8000 as *mut u8;
+    for (i, &byte) in HELLO.iter().enumerate(){
+        unsafe {
+            *vga_buf.offset(i as isize *2) = byte;
+            *vga_buf.offset(i as isize *2 + 1) = 0xb;
+        }
+    }
+    loop {}
 }
